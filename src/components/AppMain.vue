9f7f3acfe500f1b874d3a9d06127ff5dc84b85c6
1alberto994 <alby94ct@gmail.com>
2023-02-13 14:51:10 +0100

    prima sezione del main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,7 +4,7 @@
             name:'AppMain',
             data(){
                 return{
-                    
+                    backgroundImg:'src/assets/b.png'
                 }
             },
             
@@ -16,10 +16,32 @@
 
 <template>
 
-    <main>
+    <main class="mt-5">
+        <section class="first-section">
+            <img :src="backgroundImg" alt="backgroungImg">
+            <div class="container">
+                <h1>
+                    Welcome to Dogmilo pets
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, vel?</p>
+                <a href="">Get it started</a>
+            </div>
 
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
        
-       COME STAI
+       
 
     </main>
 
@@ -27,4 +49,30 @@
 
 <style lang="scss" scoped>
 
+ main{
+    min-height: 200px;
+    position: relative;
+    img{
+        object-fit: contain;
+        width: 100%;
+        
+    }
+    .container{
+       
+        position: absolute;
+        top: 0;
+        left: 0;
+        h1,p{
+            color: white;
+        }
+        a{
+            text-decoration: none;
+            background-color: #ce7c2a;
+            color: white;
+            border-radius: 10px 10px;
+            padding: 5px;
+        }
+    }
+   
+ }
 </style>
