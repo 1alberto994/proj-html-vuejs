f31c280ece93b29572ff74f73f0a377493beed8a
1alberto994 <alby94ct@gmail.com>
2023-02-13 20:47:37 +0100

    footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,6 +4,44 @@
             name:'AppFooter',
             data(){
                 return{
+                    listFooter:[
+                        {
+                            title:'Get in touch',
+                            text:"Have aquestion for us? we'll answer your problem here"
+                        },
+                        {
+                            title:'Support',
+                            arrayText:[
+                                {
+                                    lista:'Contact chat'
+                                },
+                                {
+                                    lista:'online chat'
+                                },
+                                {
+                                    lista:'Whatsapp'
+                                },
+                                {
+                                    lista:'Telegram'
+                                },
+                                {
+                                    lista:'Snapchat'
+                                }
+                            ]
+                        },
+                        {
+                            title:'About',
+                            arrayText:['About us','Blog','Carreer','Job']
+                        },
+                        {
+                            title:'Faq',
+                            arrayText:['Account','Order','Delivery','Payment','Return']
+                        },
+                        {
+                            title:'Contact me',
+                            arrayText:['via Giuseppe Garibaldi','+3910987654321','info@example.com']
+                        },
+                    ]
                     
                 }
             },
@@ -18,8 +56,24 @@
 <template>
 
     <footer>
+        <div class="d-flex form">
+            <div>
+                <img src="src/assets/news.png" alt="">
+            </div>
+            <div class="mb-3  input ">
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+        </div>
 
-       TUTTO BENE?
+      <div>
+        <ul class="d-flex ">
+            <li class="text-center" v-for="listInfo in listFooter">
+                <h3>{{listInfo.title}}</h3>
+                <a href="">{{ listInfo.text }}</a>
+                <a href="">{{ listInfo.arrayText }}</a>
+            </li>
+        </ul>
+      </div>
        
 
     </footer>
@@ -27,5 +81,38 @@
 </template>
 
 <style lang="scss" scoped>
+footer{
+    margin-top: 30px;
+    background-color: #444444;
+    position: relative;
+    .form{
+        width: 300px;
+        background-color: #ce7c2a;
+        padding: 30px;
+        border-radius: 20px;
+        position: absolute;
+        bottom: 50%;
+        left: 50%;
+        .input{
+            width: 800px;
+
+            
+        }
+        img{
+            width: 100px;
+            height:100px;
+        }
+    }
+    div{
+        max-width: 1000px;
+        margin: auto;
+    }
+    h3{
+        color: #ce7c2a;
+    }
+    ul{
+        list-style: none;
+    }
+}
 
 </style>
